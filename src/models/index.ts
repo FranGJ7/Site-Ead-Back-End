40d1bb3caa8eed1bef21e4b138553edbe5d8c6fd
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-08-10 19:26:48 -0300

    Tabela de epsódio concluida

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,15 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
                                    //associação entre tabelas 
 Category.hasMany(Course)           //Categoria curso pode ter mais associações hasMany
 Course.belongsTo(Category)         //Cursos pode pertencer a apenas uma tabela belongsTo
+Course.hasMany(Episode)
+Episode.belongsTo(Course)
 
 export{
     Category,
-    Course
+    Course, 
+    Episode
 }
