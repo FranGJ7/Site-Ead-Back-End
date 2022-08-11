7da13e258aae36cfdaae6a45c94005df262fd340
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-08-10 22:29:24 -0300

    tabela de usuários criada

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,8 +1,9 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course, Episode } from "../../models";
+import { Category, Course, Episode, User } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceFeatures, courseResourceOptions } from "./course";
 import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
+import { userResourceOptions } from "./user";
 
 export const adminJsResources: ResourceWithOptions[] = [
  
@@ -20,5 +21,9 @@ export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: categoryResourceOptions
+  },
+  {
+    resource: User,
+    options: userResourceOptions
   }
 ]                                                   //indice com todos recursos   
