d07aec74663fd3e92b8b151ba8c9ecb285c44f19
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-08-10 21:06:51 -0300

    Upload capa de cursos

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,14 +1,15 @@
 import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
-import { courseResourceOptions } from "./course";
+import { courseResourceFeatures, courseResourceOptions } from "./course";
 import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
  
   {
     resource: Course,
-    options: courseResourceOptions
+    options: courseResourceOptions,
+    features: courseResourceFeatures
   },
   {
     resource: Episode,
