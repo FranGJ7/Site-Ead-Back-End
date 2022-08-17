9f8c1923b50058adb0eff928384f6cd01eb35d29
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-08-16 23:46:09 -0300

    Login com JWT

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,35 +1,68 @@
 import { Request, Response } from "express"
+import { jwtService } from "../services/jwtService"
 import { userService } from "../services/userService"
 
-export const authController ={
-    register: async (req: Request, res: Response) => {
-        const {firstName, lastName, email, password, birth, phone} = req.body //bdy pode vir de varios formatos diferentes alterar em server
+export const authController = {
+  register: async (req: Request, res: Response) => {
+    const { firstName, lastName, email, password, birth, phone } = req.body //body pode vir de varios formatos diferentes alterar em server
 
-      try{
-           const userAlreadyExists = await userService.finByEmail(email) //pegando um email
-          
-           if(userAlreadyExists){                                 //verificar se email já existe
-              throw new Error('Este e-mail já está cadastrado')
-           }
-        
-           const user = await userService.create({
-            firstName,
-            lastName,
-            birth,
-            phone,                         //Estrutura de cração de usuário para front-end
-            email,
-            password,
-            role: 'user'
-           })
+    try {
+      const userAlreadyExists = await userService.finByEmail(email) //pegando um email
 
-           return res.status(201).json(user)
-         
-      }catch(err){if(err instanceof Error) {
-        return res.status(400).json({ message: err.message })
-    }
-
+      if (userAlreadyExists) {                                 //verificar se email já existe
+        throw new Error('Este e-mail já está cadastrado')
       }
 
+      const user = await userService.create({
+        firstName,
+        lastName,
+        birth,
+        phone,                         //Estrutura de cração de usuário para front-end
+        email,
+        password,
+        role: 'user'
+      })
 
-    } 
+      return res.status(201).json(user)
+
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+
+    }
+
+
+  },
+
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    try {
+      const user = await userService.finByEmail(email)
+
+      if (!user) return res.status(404).json({ message: 'E-mail não registrado.' })
+
+      user.checkPassword(password, (err, isSame) => {
+        if (err) return res.status(400).json({ message: err.message })
+        if (!isSame) return res.status(401).json({ message: 'Senha incorreta' })
+
+        const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email
+        }
+
+        const token = jwtService.signToken(payload, '7d')
+
+        return res.json({ authenticated: true, ...payload, token })
+      })
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+
+    }
+
+  }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,9 @@ import { sequelize } from '../database'
 import { DataTypes, Model, Optional } from 'sequelize'
 import   bcrypt from "bcrypt" 
 
+
+type checkPasswordCallBack = (err?: Error, isSame?: boolean ) => void
+
 export interface User {
   id: number
   firstName: string
@@ -17,7 +20,9 @@ export interface UserCreationAttributes
   extends Optional<User, 'id'> {}
 
 export interface UserInstance
-  extends Model<User, UserCreationAttributes>, User {}
+  extends Model<User, UserCreationAttributes>, User {
+    checkPassword: (password: string, callbackfn: checkPasswordCallBack ) => void
+  }
 
 export const User = sequelize.define<UserInstance, User>('users', {
   id: {
@@ -69,3 +74,13 @@ export const User = sequelize.define<UserInstance, User>('users', {
     }
    }
 })
+
+User.prototype.checkPassword = function (password: string, callbackfn: checkPasswordCallBack ) {
+     bcrypt.compare(password, this.password, (err, isSame) =>{
+        if(err){
+          callbackfn(err)
+        }else{
+          callbackfn(err, isSame)
+        }
+     })
+}
