e83a8a74981e609ff6fc121aa915728a10f11967
Fransérgio Gonçalves <fransergiogj7@gmail.com>
2022-08-23 18:03:14 -0300

    Obtendo informações de usúario logado

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,6 +3,20 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
+
+  
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const currentUser = req.user!
+
+    try { 
+      return res.json(currentUser)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
   // GET /users/current/watching
   watching: async (req: AuthenticatedRequest, res: Response) => {
     const { id } = req.user!
